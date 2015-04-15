2b4a1ffb2f8947fbff41dd78b79bfa8a5618bb73
Xiaoning Gao <gaox@vmware.com>
2015-04-15 04:01:59 -0700

    Signed-off-by: Xiaoning Gao <gaox@vmware.com>

--- WeChat/src/main/java/org/linuxkernel/wechat/controller/WebController.java
+++ WeChat/src/main/java/org/linuxkernel/wechat/controller/WebController.java
@@ -1,9 +1,17 @@
 package org.linuxkernel.wechat.controller;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpSession;
+
 import org.linuxkernel.wechat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,9 +21,18 @@ public class WebController extends BaseController {
 
 	@Autowired(required = true)
 	private UserService userSerivce;
-
 	@RequestMapping(value = "/web", method = RequestMethod.GET)
-	public String validate() {
+	public String welcome(Model model, HttpSession session) {
+		if (null == session.getAttribute("IP") || "".equals(session.getAttribute("IP"))) {
+			session.setAttribute("IP", getremoteIP());
+		}
+		System.out.println(session.getAttribute("IP"));
+
+		Map<String, Cookie> cookies = getAllCookie();
+		Set<Entry<String, Cookie>> set = cookies.entrySet();
+		for (Entry<String, Cookie> entry : set) {
+			System.out.println(entry.getKey() + "=" + entry.getValue().getDomain());
+		}
 		return "wechat";
 	}
 }
--- WeChat/src/main/java/org/linuxkernel/wechat/listener/SessionListener.java
+++ WeChat/src/main/java/org/linuxkernel/wechat/listener/SessionListener.java
@@ -1,19 +1,28 @@
 package org.linuxkernel.wechat.listener;
 
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 import org.apache.log4j.Logger;
+import org.linuxkernel.wechat.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class SessionListener implements HttpSessionListener {
 
 	private static Logger log = Logger.getLogger(SessionListener.class);
+
+	@Autowired(required = true)
+	private UserService userService;
+
 	public void sessionCreated(HttpSessionEvent se) {
 		log.debug("Session created");
 	}
 
 	public void sessionDestroyed(HttpSessionEvent se) {
-		log.debug("Session destroyed");
+		HttpSession session = se.getSession();
+		String ip = session.getAttribute("IP").toString();
+		long time = session.getCreationTime();
+		log.debug(ip + ":" + time + " Session Destroyed");
 	}
-
 }
